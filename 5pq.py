78b7a627fe46a7351fa1713039f8a39cdf96e1c8
eng01git <84606447+eng01git@users.noreply.github.com>
2021-06-02 09:37:33 -0300

    Update 5pq.py

--- 5pq.py
+++ 5pq.py
@@ -101,7 +101,8 @@ def load_mes(uploaded_file, tipos):
 		batch = db.batch()
 		for index, row in to_include.iterrows():
 			ref = db.collection('MES_data').document(row['documento'])
-			batch.set(ref, row.to_dict())
+			row_string = row.astype(str)
+			batch.set(ref, row_string.to_dict())
 		batch.commit()		      
 	
 	#dicionario.keys
