a1c2b7af208b501ca5891f27f692ebc0cec1f44e
eng01git <84606447+eng01git@users.noreply.github.com>
2021-06-02 16:07:25 -0300

    Update 5pq.py

--- 5pq.py
+++ 5pq.py
@@ -85,7 +85,7 @@ def load_mes():
 	mes_df = mes_df.T
 	mes_df.reset_index(inplace=True)
 	mes_df.drop('index', axis=1, inplace=True)
-	lista_colunas = ['Linha', 'Data', 'Hora', 'Tempo', 'Micro/Macro', 'Definição do Evento', 'Nome', 'Equipamento','Ponto Produtivo', 'SubConjunto', 'Componente', 'Modo de Falha - Sintoma', 'Descrição', 'Lote', 'Resultante', 'FluxoProduto', 'FluxoIntervalo', 'Turno', 'Gargalo', 'FiltroExterna']
+	lista_colunas = ['Linha', 'Data', 'Hora', 'Tempo', 'Micro/Macro', 'Definição do Evento', 'Nome', 'Equipamento','Ponto Produtivo', 'SubConjunto', 'Componente', 'Modo de Falha - Sintoma', 'Descrição', 'Lote', 'Resultante', 'FluxoProduto', 'FluxoIntervalo', 'Turno', 'Gargalo', 'FiltroExterna', 'documento']
 	mes_df = mes_df.reindex(columns=lista_colunas)
 	return mes_df
 
