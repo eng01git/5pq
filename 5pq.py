f3e2e7265492dabbf1049892a321bfa95b741630
eng01git <84606447+eng01git@users.noreply.github.com>
2021-06-04 12:50:33 -0300

    Update 5pq.py

--- 5pq.py
+++ 5pq.py
@@ -57,7 +57,7 @@ DATA_URL = "data.csv"
 				     #Definição da sidebar
 ######################################################################################################
 
-st.image('latas minas.png', width=150)
+st.sidebar.image('latas minas.png', width=150)
 st.sidebar.title("Menu 5-Porques")
 func_escolhida = st.sidebar.radio('Selecione a opção desejada',('Visibilidade', 'Inserir', 'Consultar', 'Suporte Engenharia'), index=0)
 
