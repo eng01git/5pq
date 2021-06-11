24b67da87fb366b3e1af4bbdcbe5360ccc9360d3
eng01git <84606447+eng01git@users.noreply.github.com>
2021-06-11 10:51:06 -0300

    Update 5pq.py

--- 5pq.py
+++ 5pq.py
@@ -912,7 +912,7 @@ if __name__ == '__main__':
 				
 		# Filtro para datas
 		st.text('Filtro das ações')
-		col1_ac, col2_ac, col3_ac, col4_ac = st.beta_columns(3)
+		col1_ac, col2_ac, col3_ac, col4_ac = st.beta_columns(4)
 		inicio_filtro_ac = col1_ac.date_input("Início")
 		fim_filtro_ac = col2_ac.date_input("Fim")
 		filtrado_ac = (fn_acao[(fn_acao['Prazo'] >= inicio_filtro_ac) & (fn_acao['Prazo'] <= fim_filtro_ac)]) 
