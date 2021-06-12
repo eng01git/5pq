1a0f4bb513e2d4ec9bb98ab953e03d0ac625aba6
eng01git <84606447+eng01git@users.noreply.github.com>
2021-06-12 09:45:30 -0300

    Update 5pq.py

--- 5pq.py
+++ 5pq.py
@@ -948,7 +948,7 @@ if __name__ == '__main__':
 			text = str(row['Ação']) + '' + 'Prazo: ' + str(row['Prazo'])
 			with st.beta_expander(text):
 				dados, botoes = st.beta_columns([8,2])
-				dados.table(row['Ação', 'Dono', 'Prazo', 'Gestor', 'Alerta', 'Numero do 5-Porques'])
+				dados.table(row[['Ação', 'Dono', 'Prazo', 'Gestor', 'Alerta', 'Numero do 5-Porques']])
 				botoes.button('Teste 1' + str(index))
 				botoes.button('Teste 2' + str(index))
 				botoes.button('Teste 3' + str(index))
