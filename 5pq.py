1080c59c364fa6b39b1089907caa4aa748e971f4
eng01git <84606447+eng01git@users.noreply.github.com>
2021-06-12 11:23:32 -0300

    Update 5pq.py

--- 5pq.py
+++ 5pq.py
@@ -1019,10 +1019,11 @@ if __name__ == '__main__':
 		df_people['Gestor'] = 'Não'
 		st.write(df_people)
 		st.write(usuarios_fb['Codigo'])
-		lista__ = usuarios_fb['Codigo'].tolist()
+		lista__ = usuarios_fb['Codigo'].astype(str).tolist()
 		st.write(usuarios_fb['Codigo'].dtypes)
+		
 		st.write(df_people['Codigo'].dtypes)
-		df_people2 = df_people[df_people['Codigo'].isin(lista__)]
+		df_people2 = df_people[~df_people['Codigo'].isin(lista__)]
 		
 		st.write(df_people2)
 		#for index, row in df_people.iterrows():
