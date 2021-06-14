0d419045a5dd93408481a740a4cc00025582b614
eng01git <84606447+eng01git@users.noreply.github.com>
2021-06-14 08:20:40 -0300

    Update 5pq.py

--- 5pq.py
+++ 5pq.py
@@ -993,7 +993,7 @@ if __name__ == '__main__':
 		for index, row in df_atrasadas.iterrows():
 			text = str(row['Ação']) + ' ' + ' Prazo: ' + str(row['Prazo'])
 			with st.beta_expander(text):
-				dados, botoes = st.beta_columns([9,1])
+				dados, botoes = st.beta_columns([8.5,1.5])
 				dados.table(row[['Ação', 'Dono', 'Prazo', 'Gestor', 'E-mail', 'Numero do 5-Porques']])
 
 				codigo_user = botoes.selectbox('Código do usuário', usuarios_fb['Codigo'])
