56403fa53b222d80846dc9b311e16bf0df4c3658
eng01git <84606447+eng01git@users.noreply.github.com>
2021-06-14 14:00:00 -0300

    Update 5pq.py

--- 5pq.py
+++ 5pq.py
@@ -1121,9 +1121,9 @@ if __name__ == '__main__':
 				#finalizar_acao = botoes.button('Finalizar Ação ' + str(index))
 				reabrir_acao = botoes.button('Reabrir Ação ' + str(index))
 				
-				if finalizar_acao:
-					row['Status'] = 'Concluída'
-					gravar_acao_edit(row)
+				#if finalizar_acao:
+				#	row['Status'] = 'Concluída'
+				#	gravar_acao_edit(row)
 					
 				if reabrir_acao:
 					row['Status'] = 'Em aberto'
