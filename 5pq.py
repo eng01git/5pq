34dd6a0821ad42c946b515778db268a6d3acba6c
eng01git <84606447+eng01git@users.noreply.github.com>
2021-06-18 13:55:12 -0300

    Update 5pq.py

--- 5pq.py
+++ 5pq.py
@@ -45,6 +45,8 @@ st.set_page_config(
 				#Configurando acesso ao firebase
 ######################################################################################################
 
+st.write('teste')
+
 # Pega as configurações do banco do segredo
 key_dict = json.loads(st.secrets["textkey"])
 creds = service_account.Credentials.from_service_account_info(key_dict)
