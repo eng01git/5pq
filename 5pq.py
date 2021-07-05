283510005d7ad89c9e81b3c5bd86b42ab71ab655
eng01git <84606447+eng01git@users.noreply.github.com>
2021-07-05 15:44:46 -0300

    Update 5pq.py

--- 5pq.py
+++ 5pq.py
@@ -813,8 +813,7 @@ if __name__ == '__main__':
 		st.text('Selecione a data')
 		col1, col2, col3 = st.beta_columns(3)
 		user_gest, user_resp = st.beta_columns(2)
-		st.write(dados)
-		inicio_filtro = col1.date_input("Início (ano/mês/dia)")
+		inicio_filtro = col1.date_input("Início (ano/mês/dia)", datetime.datetime(2021, 6, 1))
 		fim_filtro = col2.date_input("Fim (ano/mês/dia)")
 		filtrado = (dados[(dados['data'] >= inicio_filtro) & (dados['data'] <= fim_filtro)]) 
 
